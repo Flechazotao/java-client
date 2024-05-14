2ed7f9ff60ba65a3577e9bb0b85b0365dd6692e4
Flechazotao <1143914693@qq.com>
2024-05-14 11:14:53 +0800

    荣誉信息版面

--- java-fx/src/main/java/com/teach/javafx/controller/base/HonorController.java
+++ java-fx/src/main/java/com/teach/javafx/controller/base/HonorController.java
@@ -1,24 +1,58 @@
 package com.teach.javafx.controller.base;
 
+import com.teach.javafx.models.DO.HonorInfo;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HonorController extends manage_MainFrame_controller {
-    public TableView dataTableView;
-    public TableColumn type;
-    public TableColumn honorName;
-    public TableColumn studentName;
-    public TableColumn studentId;
-    public TableColumn level;
-    public TableColumn honorFrom;
-    public TableColumn honorTime;
-    public TableColumn file;
-    public Button onInquire;
-    public TextField InquireField;
-    public Button onAdd;
+    @FXML
+    private TextField InquireField;
+
+    @FXML
+    private TableColumn<HonorInfo, String> changeCol;
+
+    @FXML
+    private TableView <HonorInfo> dataTableView;
+
+
+    @FXML
+    private TableColumn<HonorInfo, String> deleteColumn;
+
+    @FXML
+    private TableColumn<HonorInfo, String>file;
+
+    @FXML
+    private TableColumn<HonorInfo, String> honorFrom;
+
+    @FXML
+    private TableColumn<HonorInfo, String> honorName;
+
+    @FXML
+    private TableColumn<HonorInfo, String> honorTime;
+
+    @FXML
+    private TableColumn<HonorInfo, String> level;
+
+    @FXML
+    private Button onAdd;
+
+    @FXML
+    private Button onInquire;
+
+    @FXML
+    private TableColumn<HonorInfo, String>studentId;
+
+    @FXML
+    private TableColumn<HonorInfo, String> studentName;
+
+    @FXML
+    private TableColumn<HonorInfo, String> type;
+
 
     public void onInquire(ActionEvent actionEvent) {
     }
---
+++ java-fx/src/main/java/com/teach/javafx/controller/base/Honor_Addition_Controller.java
@@ -0,0 +1,44 @@
+package com.teach.javafx.controller.base;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Honor_Addition_Controller {
+
+
+    @FXML
+    private TextField NameField;
+
+    @FXML
+    private TextField honorFromField;
+
+    @FXML
+    private DatePicker honorFromPicker;
+
+    @FXML
+    private TextField honorNameField;
+
+    @FXML
+    private TextField levelField;
+
+    @FXML
+    private Button onCancel;
+
+    @FXML
+    private TextField studentIdField;
+
+    @FXML
+    private TextField tupeField;
+    public void onCancel(ActionEvent actionEvent) {
+        Stage stage = (Stage) onCancel.getScene().getWindow();
+        stage.close();
+
+    }
+
+    public void onConformation(ActionEvent actionEvent) {
+    }
+}
