64d99568d09f169ec5c9269533bb9583e9794090
Flechazotao <1143914693@qq.com>
2024-05-14 15:23:44 +0800

    LeaveInformation

--- java-fx/src/main/java/com/teach/javafx/controller/base/LeaveInfomationController.java
+++ java-fx/src/main/java/com/teach/javafx/controller/base/LeaveInfomationController.java
@@ -1,23 +1,52 @@
 package com.teach.javafx.controller.base;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class LeaveInfomationController extends manage_MainFrame_controller {
-    public TableView dataTableView;
-    public TableColumn leaveTime;
-    public TableColumn studentId;
-    public TableColumn leaveReason;
-    public TableColumn leaveBeginTime;
-    public TableColumn leaveEndTime;
-    public TableColumn approver;
-    public TableColumn isBackSchool;
-    public Button onInquire;
-    public TextField InquireField;
-    public Button onAdd;
+    @FXML
+    private TableColumn<?, ?> ChangeCol;
+
+    @FXML
+    private TextField InquireField;
+
+    @FXML
+    private TableColumn<?, ?> approver;
+
+
+    @FXML
+    private TableView<?> dataTableView;
+
+    @FXML
+    private TableColumn<?, ?> deleteCol;
+
+    @FXML
+    private TableColumn<?, ?> isBackSchool;
+
+    @FXML
+    private TableColumn<?, ?> leaveBeginTime;
+
+    @FXML
+    private TableColumn<?, ?> leaveEndTime;
+
+    @FXML
+    private TableColumn<?, ?> leaveReason;
+
+    @FXML
+    private TableColumn<?, ?> leaveTime;
+
+    @FXML
+    private Button onAdd;
+
+    @FXML
+    private Button onInquire;
+
+    @FXML
+    private TableColumn<?, ?> studentId;
 
     public void onInquire(ActionEvent actionEvent) {
     }
