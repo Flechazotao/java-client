e37e5b9eaf099f353ed9279bd58f0cfd4b01614b
Flechazotao <1143914693@qq.com>
2024-05-15 20:04:26 +0800

    学生选课页面

--- java-fx/src/main/java/com/teach/javafx/controller/other/base/CourseSelectedS_Controller.java
+++ java-fx/src/main/java/com/teach/javafx/controller/other/base/CourseSelectedS_Controller.java
@@ -4,39 +4,265 @@ import com.teach.javafx.models.DO.Course;
 import com.teach.javafx.models.DTO.AttendanceInfoInfo;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import javafx.scene.Group;
+import javafx.scene.layout.GridPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.shape.Line;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CourseSelectedS_Controller extends manage_MainFrame_controller{
+    @FXML
+    private GridPane GridPane;
+    @FXML
+    private Button FridayFifth;
 
     @FXML
-    public TableView<Map> courseView;
-    @FXML
-    public TableColumn<Map,String> timeFixedColumn;
-    @FXML
-    public TableColumn<Map,String> week1;
+    private Button FridayFirst;
 
-    private static List<Map> courseList = new ArrayList<>();
-    private static ObservableList<Map> observableList= FXCollections.observableArrayList();
+    @FXML
+    private Button FridayFourth;
 
+    @FXML
+    private Button FridaySecond;
+
+    @FXML
+    private Button FridayThird;
+
+    @FXML
+    private Button MondayFifth;
+
+    @FXML
+    private Button MondayFirst;
+
+    @FXML
+    private Button MondayFourth;
+
+    @FXML
+    private Button MondaySecond;
+
+    @FXML
+    private Button MondayThird;
+
+    @FXML
+    private Button SaturdayFifth;
+
+    @FXML
+    private Button SaturdayFirst;
+
+    @FXML
+    private Button SaturdayFourth;
+
+    @FXML
+    private Button SaturdaySecond;
+
+    @FXML
+    private Button SaturdayThird;
+
+    @FXML
+    private Button SundayFifth;
+
+    @FXML
+    private Button SundayFirst;
+
+    @FXML
+    private Button SundayFourth;
+
+    @FXML
+    private Button SundaySecond;
+
+    @FXML
+    private Button SundayThird;
+
+    @FXML
+    private Button ThursdayFifth;
+
+    @FXML
+    private Button ThursdayFirst;
+
+    @FXML
+    private Button ThursdayFourth;
+
+    @FXML
+    private Button ThursdaySecond;
+
+    @FXML
+    private Button ThursdayThird;
+
+    @FXML
+    private Button TuesdayFifth;
+
+    @FXML
+    private Button TuesdayFirst;
+
+    @FXML
+    private Button TuesdayFourth;
+
+    @FXML
+    private Button TuesdaySecond;
+
+    @FXML
+    private Button TuesdayThird;
+
+    @FXML
+    private Button WednesdayFifth;
+
+    @FXML
+    private Button WednesdayFirst;
+
+    @FXML
+    private Button WednesdayFourth;
+
+    @FXML
+    private Button WednesdaySecond;
+
+    @FXML
+    private Button WednesdayThird;
+
+    @FXML
+    private MenuItem onAttendance;
+
+    @FXML
+    private MenuItem onCourseManage;
+
+    @FXML
+    private Button onDailyActivity;
+
+    @FXML
+    private MenuItem onFee;
+
+    @FXML
+    private MenuItem onHomework;
+
+    @FXML
+    private Button onHonor;
+
+    @FXML
+    private Button onInnovativePractice;
+
+    @FXML
+    private MenuItem onLeaveInformation;
+
+    @FXML
+    private MenuItem onScore;
 
     public void initialize() {
-        courseView.setItems(observableList);
-        timeFixedColumn.setCellValueFactory(new MapValueFactory<>("time"));
-        week1.setCellValueFactory(new MapValueFactory<>("week1"));
-        Map map1=new HashMap<>();
-        map1.put("time","第一大节");
-        map1.put("week1","高数");
-        observableList.addAll(FXCollections.observableArrayList(map1));
 
+    }
+
+    public void MondayFirst(ActionEvent actionEvent) {
+    }
+
+    public void TuesdayFirst(ActionEvent actionEvent) {
+    }
+
+    public void WednesdayFirst(ActionEvent actionEvent) {
+    }
+
+    public void ThursdayFirst(ActionEvent actionEvent) {
+    }
+
+    public void FridayFirst(ActionEvent actionEvent) {
+    }
+
+    public void SaturdayFirst(ActionEvent actionEvent) {
+    }
+
+    public void SaturdaySecond(ActionEvent actionEvent) {
+    }
+
+    public void MondaySecond(ActionEvent actionEvent) {
+    }
+
+    public void TuesdaySecond(ActionEvent actionEvent) {
+    }
+
+    public void WednesdaySecond(ActionEvent actionEvent) {
+    }
+
+    public void ThursdaySecond(ActionEvent actionEvent) {
+    }
+
+    public void FridaySecond(ActionEvent actionEvent) {
+    }
+
+    public void SundayFirst(ActionEvent actionEvent) {
+    }
+
+    public void SundaySecond(ActionEvent actionEvent) {
+    }
+
+    public void MondayThird(ActionEvent actionEvent) {
+    }
+
+    public void TuesdayThird(ActionEvent actionEvent) {
+    }
+
+    public void WednesdayThird(ActionEvent actionEvent) {
+    }
+
+    public void ThursdayThird(ActionEvent actionEvent) {
+    }
+
+    public void FridayThird(ActionEvent actionEvent) {
+    }
+
+    public void SaturdayThird(ActionEvent actionEvent) {
+    }
+
+    public void MondayFourth(ActionEvent actionEvent) {
+    }
+
+    public void SundayThird(ActionEvent actionEvent) {
+    }
+
+    public void TuesdayFourth(ActionEvent actionEvent) {
+    }
+
+    public void WednesdayFourth(ActionEvent actionEvent) {
+    }
+
+    public void ThursdayFourth(ActionEvent actionEvent) {
+    }
+
+    public void FridayFourth(ActionEvent actionEvent) {
+    }
+
+    public void SaturdayFourth(ActionEvent actionEvent) {
+    }
+
+    public void SundayFourth(ActionEvent actionEvent) {
+    }
+
+    public void TuesdayFifth(ActionEvent actionEvent) {
+    }
+
+    public void WednesdayFifth(ActionEvent actionEvent) {
+    }
+
+    public void ThursdayFifth(ActionEvent actionEvent) {
+    }
+
+    public void FridayFifth(ActionEvent actionEvent) {
+    }
+
+    public void SaturdayFifth(ActionEvent actionEvent) {
+    }
+
+    public void SundayFifth(ActionEvent actionEvent) {
+    }
+
+    public void MondayFifth(ActionEvent actionEvent) {
     }
 }
